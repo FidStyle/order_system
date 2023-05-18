aefa17e914debd5999c35beae6a6a97fa9157995
Yao Fid <115857087+FidStyle@users.noreply.github.com>
2023-05-18 12:23:53 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,25 +1,16 @@
-#include "tools.h"
-// ????
+#include "tools.h" 
+//函数声明 
 extern int main_form();
 extern int customer_form();
 extern int admin_form();
 
-int main()
-{
+int main() {
 	int choice;
-	do
-	{
+	do {
 		choice = main_form();
-		switch (choice)
-		{
-		case 1:
-			system("cls");
-			customer_form();
-			break;
-		case 2:
-			system("cls");
-			admin_form();
-			break;
+		switch (choice) {
+		case 1:system("cls"); customer_form(); break;
+		case 2:system("cls"); admin_form(); break;
 		}
 	} while (choice != 3);
 	return 0;
