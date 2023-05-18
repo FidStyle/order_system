7fbeaaadd9c362e023d3e67450d126d264ce9db7
Yao Fid <115857087+FidStyle@users.noreply.github.com>
2023-05-18 12:29:44 +0800

    Update main_form.c

--- main_form.c
+++ main_form.c
@@ -3,7 +3,7 @@ int main_form()
 {
 	int choice;
 	system("cls");
-	printf("��ӭʹ�ò������ϵͳ��\n��ѡ���������ݣ�\n1.�˿�\n2.������Ա\n3.�˳�\n\n�����������룺");
+	printf("欢迎使用餐厅点菜系统！\n请选择您的身份：\n1.顾客\n2.管理人员\n3.退出\n\n请在这里输入：");
 	choice_f('n', &choice, 1, 3);
 	return choice;
 }
