bc20674aec1f1c153663d78291a86907fd2a7d1b
Yao Fid <115857087+FidStyle@users.noreply.github.com>
2023-05-20 16:27:59 +0800

    Update main.c

--- main.c
+++ main.c
@@ -5,6 +5,14 @@ extern int customer_form();
 extern int admin_form();
 
 int main() {
+	FILE* fp;
+	if ((fp = fopen("hot_dish.txt", "r")) == NULL|| (fp = fopen("cold_dish.txt", "r")) == NULL || 
+		(fp = fopen("staple_food.txt", "r")) == NULL || (fp = fopen("drink.txt", "r")) == NULL)
+	{
+		printf("请将四个菜单txt文件与本程序放在同一目录\n");
+		my_pause();
+		return 0;
+	}
 	int choice;
 	do {
 		choice = main_form();
